c4b2e89bd984b19d743e0d05dcdeda495bb7cc60
yoojat <yooja.ty@gmail.com>
2018-08-27 00:15:02 +0900

    RideSubscription commenting

--- src/api/Ride/RideStatusSusbscription/RideStatusSusbscription.resolvers.ts
+++ src/api/Ride/RideStatusSusbscription/RideStatusSusbscription.resolvers.ts
@@ -9,6 +9,8 @@ const resolvers = {
           return pubSub.asyncIterator("rideUpdate"); //채널에 변경사항들을 푸쉬
         },
         (payload, _, { context }) => {
+          // (payload, args, { context }) => { 이처럼 args를 받아서 로직을 짤수 도 있음
+
           //payload는 전달 되는 내용
           //두번째인자는 그렇게 신경 쓰지 않아도 됨
           //context는 app.ts의 context에서 전달되어 오는 내용
@@ -16,15 +18,15 @@ const resolvers = {
           //currentUser는 listening하고 있는 사용자(driver)
           const {
             RideStatusSubscription: { driverId, passengerId }
+            //RideStatusSubscription: {id}
           } = payload;
           //payload에는 driverId와 passengerId가 담겨서 전송
           //driverId 혹은 passengerId가 subscription을 리스닝중인 유저라면
           //subscription 통해 퍼블리시를 줄수 있음
           return user.id === driverId || user.id === passengerId;
+          //return args.rideId ===id
           // 요청하는 사람들의 픽업 위치가 드라이버 근처라면 true를 리턴
 
-          // return false;
-          // return false;
           //return false;로 한다면 어떠한 업데이트도 받지 않음
         }
       )
